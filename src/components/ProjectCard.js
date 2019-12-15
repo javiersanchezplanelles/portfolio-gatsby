2f4beca272f5c39ccb35e1f2552f4291ffa92371
Javier Sanchez Planelles <javisplanelles@gmail.com>
2019-12-15 23:36:14 +0000

    Add content and styles to Project container and card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,10 +3,17 @@ import React from "react";
 function ProjectCard(props) {
     return(
         <div className="Project__Card">
-            <p>{props.name}</p>
-            <p>{props.stack}</p>
-            <p>{props.info}</p>
-        </div>
+            <a href="">
+                <div className="Project__Card__Image">
+                    <img src={props.image} alt="" />
+                </div>
+                <div className="Project__Card__Content">
+                    <p className="Project__Card__Content__Title">{props.name}</p>
+                    <p>{props.stack}</p>
+                    <p>{props.info}</p>
+                </div>
+            </a>
+        </div> 
     )
 }
 
--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -10,13 +10,21 @@ class ProjectContainer extends React.Component {
             projects: [
                 {
                     name: 'Dino app',
-                    stack: 'HTML, CSS, JS',
-                    info: 'Drag and drop the correct answer and check how much of a paleonthologist you are!'
+                    stack: 'HTML, CSS, JavaScript',
+                    info: 'Drag and drop the correct answer and check how much of a paleonthologist you are!',
+                    image: 'https://res.cloudinary.com/dovbrtmkv/image/upload/v1508778486/Portfolio/jurassicgame.png'
+                },
+                {
+                    name: 'Weather app',
+                    stack: 'HTML, CSS, JavaScript',
+                    info: 'Check the weather on this tv-like app!',
+                    image: 'https://res.cloudinary.com/dovbrtmkv/image/upload/v1496140339/Portfolio/weatherAppPic.png'
                 },
                 {
                     name: 'Todo list',
-                    stack: 'HTML, CSS, ReactJS',
-                    info: 'Your classic todo list powered with React.'
+                    stack: 'HTML, CSS, jQuery',
+                    info: 'Your classic todo list app',
+                    image: 'https://res.cloudinary.com/dovbrtmkv/image/upload/v1496140338/Portfolio/toDoList.png'
                 }
             ]
         };
@@ -26,7 +34,7 @@ class ProjectContainer extends React.Component {
         return (
             <div className="Project__Container">
                 {this.state.projects.map((project, k) => (
-                    <ProjectCard key={k} name={project.name} stack={project.stack} info={project.info} />
+                    <ProjectCard key={k} name={project.name} stack={project.stack} info={project.info} image={project.image} />
                 ))}
             </div>
         )  
