f375f36e0ae9fe6e0cc6e8aa7b41a7040e7215d7
Javier Sanchez Planelles <javisplanelles@gmail.com>
2020-01-08 22:45:45 +0000

    Update copy on About page and add margin bottom to first link on Header list on mobile

--- src/pages/About.js
+++ src/pages/About.js
@@ -17,13 +17,14 @@ function About() {
                 Working on personal projects for fun and practice is a wonderful experience that allows us 
                 to be creative and step up our game, as well as challenging our thinking too.
                 I've been having a lot of fun learning React and Gatsby for my portfolio revamp!</p>
-                <p>As I was learning to code I joined <a href="https://codebar.io/brighton">Codebar</a>, a free weekly programming workshop. It's an amazing initiave
+                <p>As I was learning to code I joined <a href="https://codebar.io/brighton">Codebar</a>, a free weekly programming workshop. It's an amazing initiative
                 which aims to help people from underrepresented groups in tech learn in a safe environment. Being part of the
                 Brighton digital community gave me the opportunity to meet like-minded people and of course learn a great deal 
                 thanks to professional coaches.</p>
-                <p>I got accepted in an internship program in a cool web agency in Brighton and then was taken on afterwards 
-                as a Front end developer, where I continue improving my skills in CSS, HTML and JavaScript.</p>
-                <p>I have come full circle and currently volunteer as a coach at Codebar, where I hope to inspire new comers to 
+                <p>After a few months with codebar I was delighted to start an internship at a creative web agency 
+                in Brighton, quickly progressing to a full-time role as Junior Front End Developer and 
+                continuing to develop my skills in CSS, HTML and JavaScript.</p>
+                <p>I have come full circle and currently volunteer as a coach at Codebar, where I hope to inspire newcomers to 
                 start a career in tech.</p>
 
                 <h2>In my free time...</h2>
